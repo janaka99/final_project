cd3bf92a89ac8fe3e061464ca825d6550db4aa91
Janaka Chamith <48000312+janaka99@users.noreply.github.com>
2022-11-21 21:56:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+
+//Import bodyparse to parse HTML body in a request
+
+//requiring routes
+
+//create mysql connection
+// const db = mysql.createConnection({
+//   host: "",
+//   user: "",
+//   password: "",
+// });
+
+//connect to mysql
+// db.connect((err) => {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log("database connected");
+// });
+
+//use bodyparser to parse HTML body in a request
+
+//setting the path
+app.set("views", path.join(__dirname, "views"));
+
+// setting the view engine
+app.set("view engine", "ejs");
+
+//setting up static path
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(3000, () => {
+  console.log("Server has started");
+});
