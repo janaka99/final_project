2b4c1d0681dc4b6ca90498ed25b4486c5425ca84
Janaka Chamith <48000312+janaka99@users.noreply.github.com>
2022-11-22 11:09:00 +0530

    first commit

--- index.js
+++ index.js
@@ -40,6 +40,10 @@ app.get("/login", (req, res) => {
   res.render("login");
 });
 
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
 app.listen(3000, () => {
   console.log("Server has started");
 });
